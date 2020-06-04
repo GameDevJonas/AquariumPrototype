e08a9393d6b3067ca52531986d574c84a4b360c4
Jonas <paandab321@gmail.com>
2020-06-04 16:09:45 +0200

    Added a more in-depth gravestone, made the shop more intuitive with different buttons

--- Assets/Scripts/FishAI.cs
+++ Assets/Scripts/FishAI.cs
@@ -20,6 +20,7 @@ public class FishAI : MonoBehaviour
 
     public GameObject graveStone;
     public GameObject[] fishesICanBe;
+    public int fishIAm;
 
     public string[] personalities;
     public string[] personalityDescription;
@@ -27,16 +28,26 @@ public class FishAI : MonoBehaviour
     public string myPersonality, myPersDesc;
 
     public CameraManager cameraMan;
+
+    public float hungerRate, affectionRate, maxHunger, currentHunger, currentAffection, sellPrice;
+    public int newSellPrice;
+    public float affectionTimer, hungerTimer;
+
+
     void Start()
     {
         graveYard = GameObject.FindGameObjectWithTag("Graveyard").GetComponent<Collider>();
-        Instantiate(fishesICanBe[Random.Range(0, fishesICanBe.Length)], transform.position, Quaternion.Euler(-90,0,90), transform);
+        int randNumb = Random.Range(0, fishesICanBe.Length);
+        fishIAm = randNumb;
+        Instantiate(fishesICanBe[randNumb], transform.position, Quaternion.Euler(-90, 0, 90), transform);
         counter = 2f;
         swimArea = GameObject.FindGameObjectWithTag("FishSpawner").GetComponent<Collider2D>();
         cameraMan = GameObject.FindObjectOfType<CameraManager>();
         int rando = Random.Range(0, personalities.Length);
         myPersonality = personalities[rando];
         myPersDesc = personalityDescription[rando];
+        currentAffection = 0;
+        currentHunger = maxHunger;
     }
 
     // Update is called once per frame
@@ -54,6 +65,61 @@ public class FishAI : MonoBehaviour
             default:
                 break;
         }
+        HungerFunc();
+        AffectionFunc();
+    }
+
+    void HungerFunc()
+    {
+        if (currentHunger <= 0)
+        {
+            OnDie();
+            cameraMan.OnZoomOut();
+        }
+
+        if (hungerTimer <= 0)
+        {
+            currentHunger--;
+            hungerTimer = hungerRate;
+        }
+        else
+        {
+            hungerTimer -= Time.deltaTime;
+        }
+    }
+
+    void AffectionFunc()
+    {
+        if (currentHunger >= (maxHunger / 2))
+        {
+            if (affectionTimer <= 0)
+            {
+                currentAffection++;
+                affectionTimer = affectionRate;
+            }
+            else
+            {
+                affectionTimer -= Time.deltaTime;
+            }
+        }
+        else
+        {
+            if (affectionTimer <= 0)
+            {
+                currentAffection--;
+                affectionTimer = affectionRate;
+            }
+            else
+            {
+                affectionTimer -= Time.deltaTime;
+            }
+        }
+    }
+
+    public void EatFood(float food)
+    {
+        currentHunger += food;
+        currentAffection += 2f;
     }
 
     public void BasicFish()
@@ -99,8 +165,18 @@ public class FishAI : MonoBehaviour
         gravePoint.z = Random.Range(graveYard.bounds.min.z, graveYard.bounds.max.z);
 
         GameObject myGrave = Instantiate(graveStone, gravePoint, Quaternion.identity);
-        myGrave.GetComponentInChildren<TextMeshProUGUI>().text = GetComponentInChildren<TextMeshProUGUI>().text;
-
+        myGrave.GetComponentInChildren<GraveStoneName>().GetComponentInChildren<TextMeshProUGUI>().text = GetComponentInChildren<TextMeshProUGUI>().text;
+        myGrave.GetComponentInChildren<GraveStonePersonality>().GetComponentInChildren<TextMeshProUGUI>().text = myPersonality;
+        myGrave.GetComponentInChildren<GraveStoneAffection>().GetComponentInChildren<TextMeshProUGUI>().text = "Affection: " + currentAffection;
+        myGrave.GetComponent<MomentumPicker>().momentums[fishIAm].SetActive(true);
+        if (currentHunger <= 0)
+        {
+            myGrave.GetComponentInChildren<GraveStoneReason>().GetComponentInChildren<TextMeshProUGUI>().text = "Died of starvation.";
+        }
+        else
+        {
+            myGrave.GetComponentInChildren<GraveStoneReason>().GetComponentInChildren<TextMeshProUGUI>().text = "Died of capitalism.";
+        }
         Destroy(gameObject);
     }
 
